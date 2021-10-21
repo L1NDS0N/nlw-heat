6c1e5f61b1354ad0fb666d955e55847ddc757ceb
Lindson França <l1nds0n@hotmail.com>
2021-10-21 17:21:13 -0300

    Header concluded

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+
+import { UserPhoto } from '../UserPhoto';
+
+import { styles } from './styles';
+import LogoSvg from '../../assets/logo.svg';
+
+export function Header() {
+  return (
+    <View style={styles.container}>
+      {/* <LogoSvg /> */}
+      <View style={styles.logoutButton}>
+        <TouchableOpacity>
+          <Text style={styles.logoutText}>Sair</Text>
+        </TouchableOpacity>
+
+        <UserPhoto imageUri="https://github.com/l1nds0n.png" />
+      </View>
+    </View>
+  );
+}
